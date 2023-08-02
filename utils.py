21eaa6d085a58e557cc54e9e47c752215f977647
FernandoPC25 <feryipalo@hotmail.com>
2023-08-02 21:10:06 +0200

    mover de svs de carpetas a una carpeta en comun

--- utils.py
+++ utils.py
@@ -28,3 +28,34 @@ folder_path = '/home/fernandopc/Documentos/prueba10/tumor/'
 output_csv_path2 = '/home/fernandopc/Documentos/prueba10/control/tumor.csv' 
 
 generate_csv_from_h5_files(folder_path, output_csv_path2, label="Tumor")
+
+
+
+import os
+import shutil
+
+# para mover todas las imagenes svs a un mismo sitio (que no estén cada una en una carpeta)
+#carpeta_principal = '/home/fernandopc/Documentos/prueba1'
+carpeta_principal = '/home/fernandopc/Escritorio/WSI-Pancreas-Classification/imagen/imagenessvs2/carpeta_patches_control'
+
+# Ruta de la carpeta donde se guardarán las imágenes .svs
+carpeta_destino = '/home/fernandopc/Escritorio/WSI-Pancreas-Classification/imagen/imagenessvs2/carpeta_patches_control'
+
+# Crear la carpeta de destino si no existe
+if not os.path.exists(carpeta_destino):
+    os.makedirs(carpeta_destino)
+
+# Recorrer las carpetas
+for carpeta in os.listdir(carpeta_principal):
+    ruta_carpeta = os.path.join(carpeta_principal, carpeta)
+
+    # Comprobar si es una carpeta
+    if os.path.isdir(ruta_carpeta):
+        # Recorrer los archivos de la carpeta
+        for archivo in os.listdir(ruta_carpeta):
+            ruta_archivo = os.path.join(ruta_carpeta, archivo)
+
+            # Comprobar si es un archivo .svs
+            if os.path.isfile(ruta_archivo) and archivo.endswith('.h5'):
+                # Copiar el archivo a la carpeta de destino
+                shutil.copy2(ruta_archivo, carpeta_destino)
